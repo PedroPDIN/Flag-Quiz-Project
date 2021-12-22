18d0201a84c491c66af89e400fe0bacc93efbf0f
Pedro <pedro.oficial1969@gmail.com>
2021-12-22 10:51:35 -0300

    feat(componets): Adiciona novos componenst: "Home", "Play", "Option", "Library".

---
+++ flag-quiz-project/src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+class Home extends Component {
+  render() {
+    return (
+             <div>
+               <h2>
+                 Seja bem vindo ao Flag Quiz!!!
+               </h2>
+               
+               <switch>
+
+               </switch>
+             </div> 
+          );
+  }
+}
+
+export default Home;
