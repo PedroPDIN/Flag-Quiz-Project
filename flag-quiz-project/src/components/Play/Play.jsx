40e88980a41825d250c1d31d0a83d8f5c4e4abd1
Pedro <pedro.oficial1969@gmail.com>
2021-12-22 14:29:58 -0300

    refactor: exclusão de detalhes minimalistas

--- flag-quiz-project/src/components/Play/Play.jsx
+++ flag-quiz-project/src/components/Play/Play.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import { Link, Route, Switch} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 class Play extends Component {
   render() {
     return (
       <div>
         <span>vc esta na Play</span>
-      
+    
         <Link to="/">Home</Link>
         </div>
     )
