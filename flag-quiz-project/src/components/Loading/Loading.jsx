66c3f7ba4044e972d155d74605eddfcab58d7052
Pedro <pedro.oficial1969@gmail.com>
2021-12-23 11:14:29 -0300

    feat(component): Adicona componente "Loading".

---
+++ flag-quiz-project/src/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Flag Quiz</h1>
+        <h3>Carregando...</h3>
+      </div>
+    )
+  }
+}
+
+export default Loading;
