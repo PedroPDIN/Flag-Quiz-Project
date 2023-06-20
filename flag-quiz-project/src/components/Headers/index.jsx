30a20be551969b9b2db4bad83547b6efc69ad330
Pedro Lima <pedro.oficial1969@gmail.com>
2023-06-20 11:16:40 -0300

    refactor(component-header): reaproveitamente do componente Header em varia páginas.

--- flag-quiz-project/src/components/Headers/index.jsx
+++ flag-quiz-project/src/components/Headers/index.jsx
@@ -51,45 +51,6 @@ class Headers extends Component {
       default:
         return <div style={{ display: "none" }}></div>;
     }
-
-    // if (typeHeader === "options") return (
-    //   <header className="container-header-option">
-    //     <Link to="/home" className="link-option">
-    //       Menu
-    //     </Link>
-    //     <h3 className="h3-option">Options</h3>
-    //   </header>
-    // );
-
-    // if (typeHeader === "play") return (
-    //   <div>
-    //     <header className="container-header-play">
-    //       <Link to="/home" className="link-menu-play">
-    //         Menu
-    //       </Link>
-    //       <h1 className="h1-play">Flag Quiz</h1>
-    //       <h3 className="score-play">{`resolved: ${score}/${numbersFlags}`}</h3>
-    //     </header>
-    //   </div>
-    // );
-
-    // if (typeHeader === "result") return (
-    //   <header className="container-header-result">
-    //     <h1 className="h1-result">Flag Quiz</h1>
-    //     <Link to="/home" className="link-menu-result">
-    //       Menu
-    //     </Link>
-    //   </header>
-    // );
-
-    // if (typeHeader === "library") return (
-    //   <header className="container-header-library">
-    //     <Link to="/menu" className="link-library">
-    //       Menu
-    //     </Link>
-    //     <h3 className="h3-library">Library</h3>
-    //   </header>
-    // );
   }
 }
 
